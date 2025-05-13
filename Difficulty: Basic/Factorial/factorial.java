9ea91f349f352319ccf97c0d1e30c939cf93c68d
Dharneesh S <ddharnish42@gmail.com>
2025-05-13 11:13:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -21,11 +21,12 @@ class GFG {
 class Solution {
     static int factorial(int n) {
         // code here
-        int sum =1;
-        for(int i=1;i<=n;i++)
-        {
-            sum = sum*i;
-        }
-        return sum;
+       if(n==0)
+       {
+           return 1;
+       }
+       return n*factorial(n-1);
+       
     }
+    
 }
