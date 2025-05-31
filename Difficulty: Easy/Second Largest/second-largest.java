ba42ff4d7e584bc38fd0faa08800b96a35563941
Dharneesh S <ddharnish42@gmail.com>
2025-05-31 21:31:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,48 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        int firstelement = arr[0];
-        int secondelement = -1;
-        for(int i=0;i<arr.length;i++)
+        int first = -1;
+        int sec =  -1;
+        for(int num : arr)
         {
-            if(arr[i]>firstelement)
+            if(first<num)
             {
-                firstelement = arr[i];
-            }
-        }
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]>secondelement && arr[i]!=firstelement)
+                sec = first;
+                first = num;
+            }else if(num>sec&&num!=first)
             {
-                secondelement =  arr[i];
+                sec = num;
             }
+        
         }
-        return secondelement;
+        return sec;
     }
 }
