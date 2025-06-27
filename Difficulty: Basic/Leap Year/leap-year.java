c5e0da66f903d5ed99d754caa187a1c77e94cac4
Dharneesh S <ddharnish42@gmail.com>
2025-06-27 11:56:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+
+class Solution {
+    static boolean isLeap(int N) {
+        // code here
+        if((N%4==0&&N%100!=0)|| (N%400==0))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
