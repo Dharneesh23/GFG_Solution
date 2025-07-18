b80b1b7b9dd215ebf7de3f60e7f8ec54e814bcad
Dharneesh S <ddharnish42@gmail.com>
2025-07-18 19:14:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,43 +1,40 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
-
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
         // code here
-        Arrays.sort(arr);
+        int zero =0;
+        int one = 0;
+        int two = 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==0)
+            {
+                zero++;
+            }
+            else if(arr[i]==1)
+            {
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        int index =0;
+        for(int i=0;i<zero;i++)
+        {
+            arr[index] = 0;
+            index++;
+        }
+        for(int i=0;i<one;i++)
+        {
+            arr[index] = 1;
+            index++;
+        }
+        for(int i=0;i<two;i++)
+        {
+            arr[index] = 2;
+            index++;
+        }
+        
     }
 }
-
-
-//{ Driver Code Starts.
-// } Driver Code Ends
