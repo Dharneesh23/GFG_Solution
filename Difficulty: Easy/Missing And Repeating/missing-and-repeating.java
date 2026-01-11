02c12238ceb6c70719bc3648cdb4cca47a5a0873
Dharneesh S <ddharnish42@gmail.com>
2026-01-11 21:33:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,26 +1,27 @@
-// User function Template for Java
-
 class Solution {
-    // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
         // code here
+        int n = arr.length;
+        int missing =-1,rep =-1;
+        int[]feq = new int[n+1];
         ArrayList<Integer>list = new ArrayList<>();
-        int n =  arr.length;
-        long nsum1 = (long)n*(n+1)/2;
-        long sqsum1 = (long)n*(n+1)*(2*n+1)/6;
-        long nsum2 = 0;
-        long sqsum2 =0;
         for(int num : arr)
         {
-            nsum2+=num;
-            sqsum2+=(long)num*num;
+            feq[num]++;
         }
-        long diff1 = nsum1-nsum2;
-        long diff2 = (sqsum1-sqsum2)/diff1;
-        long missing = (diff1+diff2)/2;
-        long repate = missing-diff1;
-        list.add((int)repate);
-        list.add((int)missing);
+        for(int i=1;i<=arr.length;i++)
+        {
+            if(feq[i]==2)
+            {
+                rep = i;
+            }
+            if(feq[i]==0)
+            {
+                missing = i;
+            }
+        }
+        list.add(rep);
+        list.add(missing);
         return list;
     }
 }
